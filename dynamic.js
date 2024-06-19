f2aec7ee89566ee1bfcf2f59e600d3c46aa8bf81
PRAGADEESHWARAN <pragadeeshpanda@gmail.com>
2024-06-19 05:01:21 -0700

    hii

---
+++ dynamic.js
@@ -0,0 +1,53 @@
+/*
+let car={
+    carname:"dodge",
+    carcolor:"black",
+    carprice:80000000
+    
+}
+for(let a in car){
+    document.write(car[a]);
+}
+
+let a=5;
+let b=6;
+let c=a+b;
+document.write(c);*/
+/*let a=15;
+if(a>18)
+    document.write("eligible to vote");
+else
+    document.write("not eligible");*/
+// let a="funvubnvn"
+// document.write("<br>",a.length);
+
+// document.write("<br>",a.slice());
+// document.write("<br>",a.substring(2,4));
+// document.write("<br>",a.substr(2,2));
+
+// document.write("<br>",a.toUpperCase());
+// document.write("<br>",a.toLowerCase());
+
+// document.write("<br>",a.replace("n","z"));
+
+// var a=new Array();
+// a[0]=21;
+// document.write(a[0]);
+
+// let animal=["lion","tiger"]
+// document.write(animal[0])
+// document.write("<br>",animal.sort())
+
+// document.write(run())
+// function run()
+// {
+//     var flower=[2,3,4,5,6];
+//     return flower
+// }
+
+let add=(a)=>{
+    console.log(a);
+}
+let a=10;
+let b=30;
+add(a)
